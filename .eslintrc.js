27e031f2ec63478d0b8e3cd2ae6c67cd077db311
AxelZapata <axelmiguelcenteno@outlook.com>
2023-05-30 01:15:29 -0600

    Eslint configurado.

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,10 +5,11 @@ module.exports = {
     tsconfigRootDir: __dirname,
     sourceType: 'module',
   },
-  plugins: ['@typescript-eslint/eslint-plugin'],
+  plugins: ['@typescript-eslint/eslint-plugin','import'],
   extends: [
+    'airbnb-typescript/base',
     'plugin:@typescript-eslint/recommended',
-    'plugin:prettier/recommended',
+    "plugin:prettier/recommended"
   ],
   root: true,
   env: {
@@ -21,5 +22,6 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-unused-vars': 'off',
   },
 };
