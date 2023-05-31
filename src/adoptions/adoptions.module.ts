13ebcf793e2891c7b0926997bf559710a55f161a
Fabian Romero <chapulinozeta@gmail.com>
2023-05-31 16:53:17 -0400

    Added providers and controller

--- src/adoptions/adoptions.module.ts
+++ src/adoptions/adoptions.module.ts
@@ -1,4 +1,10 @@
 import { Module } from '@nestjs/common';
+import { AdoptionsService } from './adoptions.service';
+import { AdoptionsController } from './adoptions.controller';
+import { adoptionsProviders } from './adoptions.providers';
 
-@Module({})
+@Module({
+  providers: [AdoptionsService, ...adoptionsProviders],
+  controllers: [AdoptionsController],
+})
 export class AdoptionsModule {}
