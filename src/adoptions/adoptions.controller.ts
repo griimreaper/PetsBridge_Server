83675b8c029137586b62f0a35812d4aa102ad844
Fabian Romero <chapulinozeta@gmail.com>
2023-06-01 13:37:53 -0400

    Add new route

--- src/adoptions/adoptions.controller.ts
+++ src/adoptions/adoptions.controller.ts
@@ -1,7 +1,9 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Post, Body, Get } from '@nestjs/common';
 import { AdoptionsService } from './adoptions.service';
 import { AdoptionDto } from './dto/adoptions.dto';
+import { ApiTags } from '@nestjs/swagger';
 
+@ApiTags('Adoptions')
 @Controller('adoptions')
 export class AdoptionsController {
   constructor(private adoptionsService:AdoptionsService) {}
@@ -9,5 +11,10 @@ export class AdoptionsController {
   @Post()
   adoptPet(@Body() IDS:AdoptionDto) {
     return this.adoptionsService.adopt(IDS);
+  }
+  
+  @Get()
+  getAdoptions() {
+    return this.adoptionsService.getAdoptions();
   }
 }
