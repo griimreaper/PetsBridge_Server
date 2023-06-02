aabeac8cbb4e1eb9546548beb40d9f5013015a5f
Fabian Romero <chapulinozeta@gmail.com>
2023-06-02 10:57:25 -0400

    Created new module and entity

---
+++ src/as_publications/entity/as_publications.entity.ts
@@ -0,0 +1,35 @@
+import { Table, Column, DataType, Model, ForeignKey, BelongsTo } from 'sequelize-typescript';
+import { Asociaciones } from 'src/asociaciones/entity/asociaciones.entity';
+
+@Table({
+  timestamps:true,
+  createdAt:true,
+  updatedAt:false,
+})
+export class AsPublication extends Model<AsPublication> {
+
+  @ForeignKey(() => Asociaciones)
+  @Column({
+    type:DataType.UUID,
+    allowNull:false,
+  })
+    ID_Asociation:string;
+
+  @Column({
+    type:DataType.STRING,
+    allowNull:false,
+  })
+    title:string;
+
+  @Column({
+    type:DataType.STRING(1000),
+    allowNull:false,
+  })
+    description:string;
+
+  @Column
+    isActive:boolean;
+
+  @BelongsTo(() => Asociaciones)
+    asociacion:Asociaciones;
+}
