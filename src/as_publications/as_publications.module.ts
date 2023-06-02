1c1f54c0ebced796741d92e97bee84377e797a41
Fabian Romero <chapulinozeta@gmail.com>
2023-06-02 11:58:11 -0400

    Added providers to Module

--- src/as_publications/as_publications.module.ts
+++ src/as_publications/as_publications.module.ts
@@ -1,4 +1,8 @@
 import { Module } from '@nestjs/common';
+import { AsPublicationsService } from './as_publications.service';
+import { AsPublicationProviders } from './providers/as_publications.providers';
 
-@Module({})
+@Module({
+  providers: [AsPublicationsService, ...AsPublicationProviders],
+})
 export class AsPublicationsModule {}
