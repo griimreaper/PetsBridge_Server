802456bd501e4a96ad4e49eac8ae578a0b2db1de
Fabian Romero <chapulinozeta@gmail.com>
2023-06-04 14:54:44 -0400

    Now postPet method can upload images and corrected deletePet method

--- src/animals/animals.service.ts
+++ src/animals/animals.service.ts
@@ -1,23 +1,34 @@
 import { Injectable, Inject, HttpException } from '@nestjs/common';
 import { Animal } from './animals.entity';
 import { AnimalDto } from './dto/animals.dto';
-import { Op } from 'sequelize/types/operators';
+import { Op } from 'sequelize';
+import { FileService } from 'src/file/file.service';
 
 @Injectable()
 export class AnimalsService {
-  constructor(@Inject('ANIMALS_REPOSITORY') private readonly animalsRepository:typeof Animal) {}
+  constructor(
+    @Inject('ANIMALS_REPOSITORY') private readonly animalsRepository:typeof Animal,
+    private readonly filesService:FileService,
+  ) {}
 
   async getPets(): Promise<Animal[]> {
     const animals = await this.animalsRepository.findAll();
     return animals;
   }
 
-  async postPet(pet:AnimalDto):Promise<string> {
+  async postPet(pet:AnimalDto, file:Express.Multer.File[]):Promise<string> {
     try {
-      await this.animalsRepository.create<Animal>(pet);
+      let urls;
+      if (file.length) urls = null;
+      urls = await this.filesService.createFiles(file);
+      await this.animalsRepository.create<Animal>({
+        ...pet,
+        images:urls,
+      });
 
       return 'Posted successfully';
     } catch (error) {
+      console.log(error.message);
       throw new HttpException(error.message, 404);
     }
   }
@@ -49,6 +60,7 @@ export class AnimalsService {
   async deletePet(id:string):Promise<string> {
     try {
       const animal = await this.animalsRepository.findByPk(id);
+      await animal.destroy();
       return 'Deleted succesfully';
     } catch (error) {
       console.log(error.message);
