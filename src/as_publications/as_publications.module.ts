06dcdf87813dfa085172cb5aa954f84773e7cb8f
Fabian Romero <chapulinozeta@gmail.com>
2023-06-03 20:45:29 -0400

    Import fileService

--- src/as_publications/as_publications.module.ts
+++ src/as_publications/as_publications.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AsPublicationsService } from './as_publications.service';
 import { AsPublicationProviders } from './providers/as_publications.providers';
 import { AsPublicationsController } from './as_publications.controller';
+import { FileService } from 'src/file/file.service';
 
 @Module({
-  providers: [AsPublicationsService, ...AsPublicationProviders],
+  providers: [AsPublicationsService, ...AsPublicationProviders, FileService],
   controllers: [AsPublicationsController],
 })
 export class AsPublicationsModule {}
