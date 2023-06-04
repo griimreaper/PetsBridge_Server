00bbad7890778af13bae6b10245317fac8760dd3
Fabian Romero <chapulinozeta@gmail.com>
2023-06-04 14:47:35 -0400

    import fileService

--- src/animals/animals.module.ts
+++ src/animals/animals.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AnimalsController } from './animals.controller';
 import { AnimalsService } from './animals.service';
 import { animalsProviders } from './animals.providers';
+import { FileService } from 'src/file/file.service';
 
 @Module({
   controllers: [AnimalsController],
-  providers: [ ...animalsProviders, AnimalsService ],
+  providers: [ ...animalsProviders, AnimalsService, FileService ],
   exports: [AnimalsService],
 })
 export class AnimalsModule {}
