144f49e72d151eee047793714269b3889d0ddf31
Fabian Romero <chapulinozeta@gmail.com>
2023-06-04 21:23:29 -0400

    Added commentsProvider

--- src/as_publications/as_publications.module.ts
+++ src/as_publications/as_publications.module.ts
@@ -3,9 +3,10 @@ import { AsPublicationsService } from './as_publications.service';
 import { AsPublicationProviders } from './providers/as_publications.providers';
 import { AsPublicationsController } from './as_publications.controller';
 import { FileService } from 'src/file/file.service';
+import { comentsProvider } from 'src/coments/comments.providers';
 
 @Module({
-  providers: [AsPublicationsService, ...AsPublicationProviders, FileService],
+  providers: [AsPublicationsService, ...AsPublicationProviders, FileService, ...comentsProvider],
   controllers: [AsPublicationsController],
 })
 export class AsPublicationsModule {}
