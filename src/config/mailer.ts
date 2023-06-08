5077e1987d8b5c34d091763457a006344f8768a7
Fabian Romero <chapulinozeta@gmail.com>
2023-06-08 12:59:04 -0400

    Mailer configuration ready

---
+++ src/config/mailer.ts
@@ -0,0 +1,25 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+import nodemailer from 'nodemailer';
+import * as dotenv from 'dotenv';
+
+
+dotenv.config();
+const { APP_EMAIL, APP_PASSWORD } = process.env;
+
+
+
+
+// create reusable transporter object using the default SMTP transport
+export const transporter = nodemailer.createTransport({
+  host: 'smtp.gmail.com',
+  port: 587,
+  secure: false, // true for 465, false for other ports
+  auth: {
+    user: APP_EMAIL, // generated ethereal user
+    pass: APP_PASSWORD, // generated ethereal password
+  },
+});
+
+transporter.verify().then(() => {
+  console.log('Server is ready to take our messages');
+}).catch(error => console.log(error));
