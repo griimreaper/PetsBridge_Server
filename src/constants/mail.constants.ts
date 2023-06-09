1f6efcf76a5519713c039de02726152113439fbc
Fabian Romero <chapulinozeta@gmail.com>
2023-06-09 16:47:28 -0400

    Changed url

--- src/constants/mail.constants.ts
+++ src/constants/mail.constants.ts
@@ -1,3 +1,3 @@
 export const mailConstants = {
-  verificationLink:'localhost:3000/algo/', //Cambia a la ruta correspondiente. Atte:Equipo Backend
+  verificationLink:'http://localhost:3000/algo/', //Cambiar por la ruta correspondiente. Atte:Equipo Backend
 };
