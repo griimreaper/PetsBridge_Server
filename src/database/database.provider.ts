200237f43556e432f20f85215106b7ff9a76b41f
AxelZapata <axelmiguelcenteno@outlook.com>
2023-06-09 06:56:14 -0600

    testeo de rama y activación de force true, todo ok

--- src/database/database.provider.ts
+++ src/database/database.provider.ts
@@ -33,7 +33,7 @@ export const databaseProviders = [
         RedSocial,
       ]);
       try {
-        await sequelize.sync({ force: false });
+        await sequelize.sync({ force: true });
       } catch (error) {
         console.error('SEQUALIZE SYNC ERROR: ', error);
       }
