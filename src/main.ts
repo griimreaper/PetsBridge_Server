ade3b237d085804542d4687b30089ed15a510a83
Fabian Romero <chapulinozeta@gmail.com>
2023-06-10 10:16:41 -0400

    Configured pug

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,12 @@ import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import * as morgan from 'morgan';
 import { CORS } from './constants';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
 const port = process.env.SERVER_PORT || 3001;
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
   const options = new DocumentBuilder()
     .addBearerAuth()
     .setTitle('PetsBridge')
@@ -27,6 +29,14 @@ async function bootstrap() {
       showRequestDuration: true,
     },
   });
+
+  //Configuring pug for email templates
+  /* Telling nest we will use ../public folder for storing static assets (e.g. images) */
+  app.useStaticAssets(join(__dirname, '..', 'public'));
+  /* Telling nest we will use ../views folder for storing templates */
+  app.setBaseViewsDir(join(__dirname, '..', 'views'));
+  /* Telling nest we will use 'pug' as our template engine*/
+  app.setViewEngine('pug');
 
 
   await app.listen(port);
