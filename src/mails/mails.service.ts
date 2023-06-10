f186fb4e9d2b1c4a6b8da94841f88247dcb6d31c
Fabian Romero <chapulinozeta@gmail.com>
2023-06-10 13:08:59 -0400

    Send mails with resetPassword template

--- src/mails/mails.service.ts
+++ src/mails/mails.service.ts
@@ -1,10 +1,9 @@
 /* eslint-disable @typescript-eslint/quotes */
 import { Injectable } from '@nestjs/common';
-import { transporter } from 'src/config/mailer';
+import { transporter, email } from 'src/config/mailer';
 import { ConfigService } from '@nestjs/config';
 import { Users } from 'src/users/entity/users.entity';
 import { Asociaciones } from 'src/asociaciones/entity/asociaciones.entity';
-import { mailConstants } from 'src/constants/mail.constants';
 
 @Injectable()
 export class MailsService {
@@ -13,67 +12,18 @@ export class MailsService {
   AppEmail = this.configService.get('APP_EMAIL');
 
   async sendMails(data, topic:string) {
-
+    console.log(data);
     switch (topic) {
-      //When reset Password
       case 'RESET_PASSWORD':
+        const html = await email.render('password/resetPassword', { username:data.firstName, token: data.reset });
         await transporter.sendMail({
-          from: this.AppEmail, // sender address
-          to: data.email, // list of receivers
-          subject: 'Cambio de contraseña', // Subject line
-          text:`¡Saludos!</n>
-           Usa el siguiente token para crear una nueva </n> contraseña: </n>
-           ${data.reset} </n>
-           No responda a este remitente.</n>
-           Att: Equipo de PetsBridge.
-          `,
-          html: `
-          <body style='font-family: ‘Roboto’, Helvetica, Arial, sans-serif; text-align:center;'>
-            <h1 style='color:purple; background-color:yellow;'>¡Saludos!</h1> 
-              <p>Usa el siguiente token para crear una nueva <br/> contraseña:</p>
-              <p style='background-color:gray;'>${data.reset}</p><br/><br/>
-              <footer style='text-align:left;'>
-              <p>No responda a este remitente.</p>
-              <p>Att: Equipo de <b><i>PetsBridge</i></b></p>
-              </footer>
-          </body>`, // html body
+          to:data.email,
+          subject:'Cambio de contraseña',
+          html:html,
         });
         break;
-
-        //When register
-      case 'REGISTER':
-        await transporter.sendMail({
-          from: this.AppEmail, // sender address
-          to: data.email, // list of receivers
-          subject: 'Verificación', // Subject line
-          text:`¡Saludos!</n>
-             Muchas gracias por elegir ser parte de nuestra comunidad</n>
-             Pero antes que nada, verifiquemos tu cuenta.</n>
-             Copia y pega la siguiente url en tu navegador</n>
-             ${mailConstants.verificationLink}${data.code}/${data.id}</n>
-             No responda a este remitente.</n>
-             Att: Equipo de PetsBridge.
-            `,
-          html: `
-            <body style='font-family: ‘Roboto’, Helvetica, Arial, sans-serif; text-align:center;'>
-              <h1 style='color:purple; background-color:yellow;'>¡Saludos!</h1> 
-                <p>Muchas gracias por elegir ser parte de nuestra comunidad</p>
-                <p>Pero antes que nada, verifiquemos tu cuenta haciendo click en el siguiente enlace.</p>
-
-                <a style='background-color:gray;' href=${mailConstants.verificationLink}${data.code}/${data.id}>
-                Link de verificación
-                </a>
-
-                <br/>
-                <br/>
-                <footer style='text-align:left;'>
-                <p>No responda a este remitente.</p>
-                <p>Att: Equipo de <b><i>PetsBridge</i></b></p>
-                </footer>
-            </body>`, // html body
-        });
-        break;
+      default:
+        return;
     }
-    
   }
 }
