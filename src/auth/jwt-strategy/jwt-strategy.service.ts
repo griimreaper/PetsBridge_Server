b45cf2b11857bbf77351dacebcbd64e81a12c0c0
Fabian Romero <chapulinozeta@gmail.com>
2023-06-10 20:20:52 -0400

    Implemented a validate method

--- src/auth/jwt-strategy/jwt-strategy.service.ts
+++ src/auth/jwt-strategy/jwt-strategy.service.ts
@@ -1,11 +1,16 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
 import { jwtConstanst } from '../../constants/jwt.constants';
+import { UsersService } from 'src/users/users.service';
+import { AsociacionesService } from 'src/asociaciones/asociaciones.service';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
-  constructor() {
+  constructor(
+    private readonly usersService:UsersService,
+    private readonly asociacionesService:AsociacionesService,
+  ) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
@@ -13,7 +18,22 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
     });
   }
 
-  async validate(payload: any) {
-    return { id: payload.sub, email: payload.email, rol: payload.rol };
+  async validate(payload:any) {
+    let user;
+    let asociacion;
+    let admin;
+    switch (payload.rol) {
+      case 'user':
+        user = await this.usersService.findById(payload.id);
+      case 'fundation':
+        asociacion = await this.asociacionesService.findOne(payload.id);
+      case 'admin':
+        admin = await this.usersService.findById(payload.id);
+    }
+    if (!user && !asociacion && !admin) throw new UnauthorizedException('You are not authorized to perform the operation');
+    return payload;
   }
+  /* async validate(payload: any) {
+    return { id: payload.sub, email: payload.email, rol: payload.rol };
+  } */
 }
