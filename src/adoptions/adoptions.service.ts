2b917b188d63f8ed02f1a7091fb2643ba2701fce
Fabian Romero <chapulinozeta@gmail.com>
2023-06-11 10:50:35 -0400

    Send emails every time a pet is adopted

--- src/adoptions/adoptions.service.ts
+++ src/adoptions/adoptions.service.ts
@@ -1,9 +1,10 @@
-import { Injectable, Inject, HttpException, HttpStatus } from '@nestjs/common';
+import { Injectable, Inject, HttpException, HttpStatus, NotFoundException } from '@nestjs/common';
 import { Adoption } from './adoptions.entity';
 import { AdoptionDto } from './dto/adoptions.dto';
 import { Users } from 'src/users/entity/users.entity';
 import { Animal } from 'src/animals/animals.entity';
 import { MailsService } from 'src/mails/mails.service';
+import { NotFoundError } from 'rxjs';
 
 @Injectable()
 export class AdoptionsService {
@@ -23,13 +24,29 @@ export class AdoptionsService {
           userID: user.id,
           animalID: animal.id,
         });
-        this.mailsService.sendMails(adoption, 'ADOPT');
+        const adoptionData = await this.findById(adoption.dataValues.id);
+        this.mailsService.sendMails(adoptionData, 'ADOPT');
         return 'Adopted successfully';
       } else {
         throw new HttpException('User or animal not found', HttpStatus.NOT_FOUND);
       }
     } catch (error) {
       throw new HttpException('Adoption failed', HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+  }
+
+  async findById(id:string):Promise<Adoption | NotFoundException> {
+    try {
+      const adoption = await this.adoptionsRepository.findOne({
+        where: {
+          id: id,
+        },
+        include: [Animal, Users],
+      });
+      if (!adoption) throw new NotFoundException('Adoption not found');
+      return adoption;
+    } catch (error) {
+      return error;
     }
   }
 }
