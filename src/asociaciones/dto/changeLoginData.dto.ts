c66976a3ef15067ba371714f8af3575b48ab138b
Fabian Romero <chapulinozeta@gmail.com>
2023-06-18 00:13:38 -0400

    Agregué un changeEmailDto

--- src/asociaciones/dto/changeLoginData.dto.ts
+++ src/asociaciones/dto/changeLoginData.dto.ts
@@ -1,5 +1,11 @@
 export interface ChangePasswordDto {
   id:string,
   oldPassword:string,
-  newPassword:string,
+  newPassword:string
+}
+
+export interface ChangeEmailDto {
+  id:string,
+  newEmail:string,
+  password:string,
 }
