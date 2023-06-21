a8601e2112cdfad56010d2b0a59021386f3e0be4
Fabian Romero <chapulinozeta@gmail.com>
2023-06-21 16:01:21 -0400

    Imported modules

--- src/reviews/reviews.module.ts
+++ src/reviews/reviews.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ReviewsService } from './reviews.service';
 import { reviewsProviders } from './reviews.provider';
+import { UsersModule } from '../users/users.module';
+import { ReviewsController } from './reviews.controller';
+import { AsociacionesModule } from 'src/asociaciones/asociaciones.module';
 
 @Module({
+  imports:[UsersModule, AsociacionesModule],
   providers: [ReviewsService, ...reviewsProviders ],
+  controllers: [ReviewsController],
 })
 export class ReviewsModule {}
