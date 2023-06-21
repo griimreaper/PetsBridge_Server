380288ce837c0af0818927ca89a2e1c2700db865
Fabian Romero <chapulinozeta@gmail.com>
2023-06-21 16:01:47 -0400

    Added new providers to reviewsService

--- src/reviews/reviews.service.ts
+++ src/reviews/reviews.service.ts
@@ -2,16 +2,41 @@ import { BadRequestException, HttpException, Inject, Injectable, NotFoundExcepti
 import { Review } from './reviews.entity';
 import { ReviewsDto } from './dto/reviews.dto';
 import { ConditionalReviewsDto } from './dto/reviews.dto';
+import { UsersService } from '../users/users.service';
 import { Users } from '../users/entity/users.entity';
+import { AsociacionesService } from '../asociaciones/asociaciones.service';
+import { Asociaciones } from '../asociaciones/entity/asociaciones.entity';
 
 @Injectable()
 export class ReviewsService {
-  constructor(@Inject('REVIEWS_REPOSITORY') private reviewsRepository: typeof Review) {}
+  constructor(
+    @Inject('REVIEWS_REPOSITORY') private reviewsRepository: typeof Review,
+    private usersService:UsersService,
+    private asociacionesService: AsociacionesService,
+  ) {}
 
   async review(review:ReviewsDto):Promise<Review | HttpException> {
     try {
+      const { idUser, idAsociacion } = review;
+      let user;
+      let asociacion;
+
+      try {
+        user = await this.usersService.findById(idUser);
+      } catch (error) {
+        console.log(error.message);
+      }
+      
+      try {
+        asociacion = await this.asociacionesService.findOne(idAsociacion);
+      } catch (error) {
+        console.log(error.message);
+      }
+  
+      if (!user && !asociacion) throw new NotFoundException('No se encontró al usuari');
+
       const newReview = await this.reviewsRepository.create({ ...review });
-      if (newReview) throw new BadRequestException('No se encontró al usuario');
+      if (!newReview) throw new BadRequestException('Algo salió mal');
 
       return newReview;
     } catch (error) {
@@ -22,7 +47,7 @@ export class ReviewsService {
   async editReview(id:string, newValues:ConditionalReviewsDto) {
     try {
       const review = await this.reviewsRepository.findByPk(id);
-      if (review) throw new NotFoundException('No existe ese review');
+      if (!review) throw new NotFoundException('No existe ese review');
 
       await review.update({ ...newValues }, {
         where:{
@@ -42,10 +67,15 @@ export class ReviewsService {
 
   async getAllReviews():Promise<Review[] | HttpException> {
     try {
-      const allReviews = await this.reviewsRepository.findAll({ include: {
+      const allReviews = await this.reviewsRepository.findAll({ include: [{
         model:Users,
         attributes:['firstName', 'lastName'],
       },
+      {
+        model:Asociaciones,
+        attributes:['nameOfFoundation'],
+      },
+      ],
       });
       return allReviews;
 
