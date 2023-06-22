5095d40e3c404f07a6dd6d54f72c2295bad0a782
griimreaper <leonelbehnke@gmail.com>
2023-06-22 07:16:42 -0300

    // allowedHeaders //

--- src/constants/cors.ts
+++ src/constants/cors.ts
@@ -1,7 +1,8 @@
 import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 
 export const CORS: CorsOptions = {
-  origin: '*',
+  origin: 'https://petbridge.vercel.app', // o tu lista de orígenes permitidos
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
   credentials: true,
+  allowedHeaders: 'Content-Type,Authorization',
 };
