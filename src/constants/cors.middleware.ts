386036b686c10bc48595397d420a6ea95d8bc66f
griimreaper <leonelbehnke@gmail.com>
2023-06-22 15:58:59 -0300

    // cors actualizado

--- src/constants/cors.middleware.ts
+++ src/constants/cors.middleware.ts
@@ -4,10 +4,7 @@ import { Request, Response, NextFunction } from 'express';
 @Injectable()
 export class CorsMiddleware implements NestMiddleware {
   use(req: Request, res: Response, next: NextFunction) {
-    res.header('Access-Control-Allow-Origin', 'https://petbridge.vercel.app');
-    res.header('Access-Control-Allow-Origin', 'https://pet-bridge.vercel.app');
-    res.header('Access-Control-Allow-Origin', 'http://localhost:3001');
-    res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.header('Access-Control-Allow-Origin', ['https://petbridge.vercel.app', 'https://pet-bridge.vercel.app', 'http://localhost:3001', 'http://localhost:3000']);
     res.header(
       'Access-Control-Allow-Methods',
       'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
