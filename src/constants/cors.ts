e30077da26aa04b39f273707a78163c709add773
griimreaper <leonelbehnke@gmail.com>
2023-06-22 04:35:26 -0300

    // Cors modificado //

--- src/constants/cors.ts
+++ src/constants/cors.ts
@@ -1,7 +1,7 @@
 import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 
 export const CORS: CorsOptions = {
-  origin: true,
+  origin: '*',
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
   credentials: true,
 };
