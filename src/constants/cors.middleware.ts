fbc484c7540f348477cb040d194dff5f841bc962
griimreaper <leonelbehnke@gmail.com>
2023-06-23 01:14:21 -0300

    ..

--- src/constants/cors.middleware.ts
+++ src/constants/cors.middleware.ts
@@ -4,8 +4,11 @@ import { Request, Response, NextFunction } from 'express';
 @Injectable()
 export class CorsMiddleware implements NestMiddleware {
   use(req: Request, res: Response, next: NextFunction) {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
+    res.header('Access-Control-Allow-Origin', '*'); // Establece el encabezado de Access-Control-Allow-Origin a '*'
+    res.header(
+      'Access-Control-Allow-Methods',
+      'GET, HEAD, PUT, PATCH, POST, DELETE, OPTIONS',
+    );
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     res.header('Access-Control-Allow-Credentials', 'true');
 
@@ -13,7 +16,7 @@ export class CorsMiddleware implements NestMiddleware {
       // Respond to preflight requests
       res.sendStatus(200);
     } else {
-      // Pass the request to the next middleware
+      // Pasar la solicitud al siguiente middleware
       next();
     }
   }
